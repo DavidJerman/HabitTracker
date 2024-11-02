d3459c973ae035788e5f57e5ba606150b6a144f7
IronLike <enej.sercer1@gmail.com>
2024-11-02 02:22:01 +0100

    zacetek

--- server/src/models/Meal.js
+++ server/src/models/Meal.js
@@ -3,12 +3,55 @@
 /**
  * @module Meal
  * @description Meal Schema represents a user's meal in the MongoDB database.
- * TODO: Add properties to the Meal Schema
  */
 
 const mongoose = require('mongoose');
 
+/**
+ * @typedef {object} Meal
+ *
+ * @property {string} userId - The ID of the user associated with the meal.
+ * @property {string} name - The name of the meal (required).
+ * @property {string} [notes] - A brief description of the meal.
+ * @property {Date} dateAdded - The date when the meal was added (default: current date).
+ * @property {('breakfast'|'lunch'|'dinner'|'snack'|'other')} mealType - The type of meal.
+ */
+
+/**
+ * Task Schema for Mongoose.
+ *
+ * @type {mongoose.Schema<Meal>}
+ */
 const MealSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: True,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+        required: false,
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    ingredients: [{
+        ingredient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Ingredient',
+            required: True,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+    }]
 });
 
 module.exports = mongoose.model('Meal', MealSchema);
--- server/src/routes/nutrition.js
+++ server/src/routes/nutrition.js
@@ -3,4 +3,25 @@
 /**
  * @module Nutrition
  * @description Nutrition Router provides endpoints for creating, deleting, and updating nutrition entries.
+ * TODO VSE
  */
+
+const express = require('express');
+const router = express.Router();
+const Meal = require('../models/Meal');
+const Ingredient = require('../models/Ingredient');
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
+
