77a162a1141c74b6c38ca456e11e9fa74dd8333d
David Jerman <davidjermna3@gmail.com>
2024-11-04 16:54:48 +0100

    Added register test

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+require('./test_auth')
+
+describe('Main Test Suite', () => {
+    it('should run all tests', () => {
+        console.log('All tests are running');
+    });
+});
---
+++ tests/test_auth.js
@@ -0,0 +1,143 @@
+const request = require('supertest');
+const app = 'http://localhost:3000'; // The URL of your API
+
+// Function to generate a random string
+const generateRandomString = (length) => {
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
+
+describe('Auth API', () => {
+    describe('POST /auth/register', () => {
+        it('should register a new user', async () => {
+            const username = `testuser_${generateRandomString(5)}`;
+            const email = `${generateRandomString(8)}@mail.com`;
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    password: 'password',
+                    email: email
+                });
+
+            expect(response.status).toBe(201);
+            expect(response.body).toHaveProperty('message', 'User registered');
+        });
+
+        it('should return an error for missing username', async () => {
+            const email = `${generateRandomString(8)}@mail.com`;
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send({
+                    password: 'password',
+                    email: email
+                });
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('error', 'Missing required user information');
+        });
+
+        it('should return an error for missing password', async () => {
+            const username = `testuser_${generateRandomString(5)}`;
+            const email = `${generateRandomString(8)}@mail.com`;
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    email: email
+                });
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('error', 'Missing required user information');
+        });
+
+        it('should return an error for missing email', async () => {
+            const username = `testuser_'${generateRandomString(5)}`;
+            const email = ``;
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    password: 'password'
+                });
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('error', 'Missing required user information');
+        });
+
+        it('should not return an error for extra fields', async () => {
+            const username = `testuser_${generateRandomString(5)}`;
+            const email = `${generateRandomString(8)}@mail.com`;
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    password: 'password',
+                    email: email,
+                    extraField: 'extraValue' // This is an extra field
+                });
+
+            expect(response.status).toBe(201);
+            expect(response.body).toHaveProperty('message', 'User registered');
+        });
+
+        it('should return an error for user existing', async () => {
+            const username = `testuser_${generateRandomString(5)}`;
+            const email = `${generateRandomString(8)}@mail.com`;
+
+            let response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    password: 'password',
+                    email: email
+                });
+
+            response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    password: 'password',
+                    email: email
+                });
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('error', 'User already exists');
+        });
+
+        it('should return an error for email existing', async () => {
+            let username = `testuser_${generateRandomString(5)}`;
+            const email = `${generateRandomString(8)}@mail.com`;
+
+            let response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    password: 'password',
+                    email: email
+                });
+
+            username = `testuser_${generateRandomString(5)}`;
+
+            response = await request(app)
+                .post('/auth/register')
+                .send({
+                    username: username,
+                    password: 'password',
+                    email: email
+                });
+
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('error', 'Email already exists');
+        })
+    });
+});
