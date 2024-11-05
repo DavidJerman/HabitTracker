9ec94e98baf2079577f4ce74ca6b7f30591ce83c
David Jerman <davidjerman3@gmail.com>
2024-11-05 17:54:35 +0100

    Implemented activity tests

--- tests/server/test_tasks.js
+++ tests/server/test_tasks.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = 'http://localhost:3000'; // The URL of your API
+const app = 'http://localhost:3000';
 
 // Function to generate a random string
 const generateRandomString = (length) => {
--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,6 @@
 require('./server/test_auth')
 require('./server/test_tasks')
+require('./server/test_activities')
 
 describe('Main Test Suite', () => {
     it('should run all tests', () => {
